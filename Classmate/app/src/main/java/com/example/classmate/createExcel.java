6418835d7e3d0f0ed98ac55db253e8c4fa004969
HBING <598003677@qq.com>
2017-11-05 02:08:50 +0800

    fix

--- Classmate/app/src/main/java/com/example/classmate/createExcel.java
+++ Classmate/app/src/main/java/com/example/classmate/createExcel.java
@@ -5,11 +5,7 @@ import android.os.Environment;
 import android.widget.Toast;
 
 import com.example.classmate.item.Person;
-
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
 
 import jxl.Cell;
